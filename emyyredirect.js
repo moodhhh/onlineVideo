295ea5f96af31e617226d7928a6d7708403d072a
moodhhh <hiranshalikafdo@gmail.com>
2025-05-04 21:38:58 +0530

    Create emyyredirect.js

---
+++ emyyredirect.js
@@ -0,0 +1,6 @@
+
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://sawutser.top/4/9286605");  // Redirect to the URL
+        }, 20000);  // 3000 milliseconds = 3 seconds
